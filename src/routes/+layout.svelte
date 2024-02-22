a6902f62ba20a5d31805b9b54bdd3019504a5c14
decefemz <36079094+decefemz@users.noreply.github.com>
2024-02-22 19:57:39 +0200

    feat: add shadcn ui

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import Header from "./_components/header";
+    import "../app.pcss";
+</script>
+<Header/>
+<slot/>
---
+++ src/routes/_components/header/header.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import {Button} from "$lib/ui/button"
+</script>
+
+<header class="h-14 border-b border-border p-2 px-4">
+    <nav>
+        <Button variant="ghost">
+            Projects
+        </Button>
+    </nav>
+	
+</header>
